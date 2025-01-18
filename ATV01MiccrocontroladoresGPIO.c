594559d76bb7dac91c785475315442cf0a21e0e2
Danilo.Lacerda <daanlacerda@hotmail.com>
2025-01-18 11:14:51 -0300

    Atualização das portas GPIO para conexão do teclado

--- ATV01MiccrocontroladoresGPIO.c
+++ ATV01MiccrocontroladoresGPIO.c
@@ -4,12 +4,12 @@
 
 #define ROWS 4
 #define COLS 4
-uint8_t row_pins[ROWS] = {2, 3, 4, 5};  
-uint8_t col_pins[COLS] = {6, 7, 8, 9};  
+uint8_t row_pins[ROWS] = {16, 17, 28, 18};  
+uint8_t col_pins[COLS] = {19, 20, 4, 9};  
 
 #define LED_RED 11
 #define LED_GREEN 12
-#define LED_BLUE 13
+#define LED_BLUE 13                           
 #define BUZZER_PIN 21
 
 const char keys[ROWS][COLS] = {
